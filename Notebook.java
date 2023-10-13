22841ccd9355b948a701c747e05699147c4149c3
Radmir Shagiev <veselday@gmail.com>
2023-10-13 18:31:53 +0500

    create variables

---
+++ Notebook.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Laptop {
+    private String model;
+    private int ram;
+    private int hdd;
+    private String os;
+    private String color;
+
+    public Laptop(String model, int ram, int hdd, String os, String color) {
+        this.model = model;
+        this.ram = ram;
+        this.hdd = hdd;
+        this.os = os;
+        this.color = color;
+    }
+
+}
